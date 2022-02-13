22161a1e2ac6854dcd2519390a35e0627a72a94c
Tobias Rose <tobias.rose@ukbonn.de>
2022-02-13 09:19:08 +0100

    testbed

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,20 @@
+# User config setting for Point Grey Bonsai multi camera acquisition.
+
+# Experiment config.
+
+subjects = {1:'m1',  # {box: subject_ID}
+            2:'m2',
+                    }
+
+data_dir = 'D:\\video_test'  # Directory where data will be saved.
+
+# Hardware config.
+
+camera_IDs = {1: 0,      # {box: camera_index}
+              2: 1}
+
+# Bonsai path config.
+
+bonsai_path = '"C:\Users\mini2p_1_aux\AppData\Local\Bonsai\Bonsai.exe"'
+
+workflow_path = 'C:\Users\mini2p_1_aux\Documents\GitHub\Basler_Bonsai_multi_camera_acquisition'
