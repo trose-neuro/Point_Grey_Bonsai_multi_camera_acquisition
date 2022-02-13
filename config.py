8a0fbd61dd3ae10949fdade6952d9bb04b575c79
Tobias Rose <tobias.rose@ukbonn.de>
2022-02-13 08:53:26 +0100

    config change

--- config.py
+++ config.py
@@ -4,17 +4,14 @@
 
 subjects = {1:'m1',  # {box: subject_ID} 
             2:'m2',
-            3:'m3',
-            4:'m4'}
+                    }
 
 data_dir = 'D:\\video_test'  # Directory where data will be saved.
 
 # Hardware config.
 
 camera_IDs = {1: 0,      # {box: camera_index}
-              2: 1,
-              3: 2,
-              4: 3}
+              2: 1}
 
 # Bonsai path config.
 
